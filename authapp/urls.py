0ea8b56189abe919b2648f581d884c1fed9321c1
trivendra-501 <trivendrat501@gmail.com>
2024-07-10 12:44:17 +0530

    Delete authapp directory

--- authapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from authapp import views
-
-urlpatterns = [
-    path('signup/',views.signup, name="signup"),
-    path('login/',views.handleLogin, name="handleLogin"),
-    path('logout/',views.handleLogout, name="handleLogout"),
-
-]
